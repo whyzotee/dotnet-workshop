4ba9cded6063b5a72f0d3c97841e48ad75a9e4cd
Z0TEExt <zenlektomyum@gmail.com>
2022-12-24 13:08:22 +0700

    .

---
+++ Lab3/.Net-Lab3_6.4/Lab3_6.4/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Lab3_6._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Kanit", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 86);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "โปรแกรมแปลงอัตราแลก\r\nเปลี่ยนเงินตราต่างประเทศ";
+            // 
+            // comboBox
+            // 
+            this.comboBox.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "USD (ดอลลาร์สหรัฐ)",
+            "JPY (เยน)",
+            "EUR (ยูโร)",
+            "SGD (ดอลลาร์สิงคโปร์)",
+            "LAK (กีบ)",
+            "TWD (ดอลลาร์ไต้หวันใหม่)"});
+            this.comboBox.Location = new System.Drawing.Point(148, 138);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(201, 41);
+            this.comboBox.TabIndex = 1;
+            this.comboBox.Text = "USD (ดอลลาร์สหรัฐ)";
+            // 
+            // num1
+            // 
+            this.num1.Font = new System.Drawing.Font("Kanit", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num1.Location = new System.Drawing.Point(49, 247);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(201, 36);
+            this.num1.TabIndex = 2;
+            this.num1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // num2
+            // 
+            this.num2.Font = new System.Drawing.Font("Kanit", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num2.Location = new System.Drawing.Point(49, 304);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(201, 36);
+            this.num2.TabIndex = 3;
+            this.num2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(43, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(235, 33);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "จำนวนเงินที่ต้องการแปลง";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(256, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 33);
+            this.label3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(256, 307);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 33);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "THB (บาท)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(264, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 36);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "แปลงค่าเงิน";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(43, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 33);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "สกุลเงิน";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(418, 367);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "baht_convert";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Lab3/.Net-Lab3_6.4/Lab3_6.4/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab3_6._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (comboBox.SelectedIndex) 
+            {
+                case 0:
+                    num2.Text = (float.Parse(num1.Text) * 34.73).ToString();
+                    break;
+                case 1:
+                    num2.Text = (float.Parse(num1.Text) * 0.26).ToString();
+                    break;
+                case 2:
+                    num2.Text = (float.Parse(num1.Text) * 37.00).ToString();
+                    break;
+                case 3:
+                    num2.Text = (float.Parse(num1.Text) * 25.75).ToString();
+                    break;
+                case 4:
+                    num2.Text = (float.Parse(num1.Text) * 0.0020).ToString();
+                    break;
+                case 5:
+                    num2.Text = (float.Parse(num1.Text) * 1.14).ToString();
+                    break;
+            }
+            
+        }
+    }
+}
