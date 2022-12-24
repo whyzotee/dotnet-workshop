93686da6a53b650bcba7e3a167cd6a213672e655
Z0TEExt <zenlektomyum@gmail.com>
2022-12-24 13:05:45 +0700

    .

---
+++ Lab3/.Net-Lab3_6.5/Lab3_6.5/Form1.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace Lab3_6._5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.num4 = new System.Windows.Forms.TextBox();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.num3 = new System.Windows.Forms.TextBox();
+            this.num5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.seri = new System.Windows.Forms.RadioButton();
+            this.para = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.ans = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(48, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 66);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "โปรแกรมหาค่าความต้านทานรวม\r\nของตัวต้านทานอนุกรมและขนาน";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(25, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "เลือกโหมด";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(25, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 33);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ค่า 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(287, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 33);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "ค่า 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(25, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 33);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "ค่า 2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(25, 281);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 33);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "ค่า 3";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(287, 223);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 33);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "ค่า 5";
+            // 
+            // num1
+            // 
+            this.num1.Font = new System.Drawing.Font("Kanit", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num1.Location = new System.Drawing.Point(97, 164);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(142, 36);
+            this.num1.TabIndex = 8;
+            this.num1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // num4
+            // 
+            this.num4.Font = new System.Drawing.Font("Kanit", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num4.Location = new System.Drawing.Point(359, 164);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(142, 36);
+            this.num4.TabIndex = 9;
+            this.num4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // num2
+            // 
+            this.num2.Font = new System.Drawing.Font("Kanit", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num2.Location = new System.Drawing.Point(97, 222);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(142, 36);
+            this.num2.TabIndex = 10;
+            this.num2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // num3
+            // 
+            this.num3.Font = new System.Drawing.Font("Kanit", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num3.Location = new System.Drawing.Point(97, 281);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(142, 36);
+            this.num3.TabIndex = 11;
+            this.num3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // num5
+            // 
+            this.num5.Font = new System.Drawing.Font("Kanit", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num5.Location = new System.Drawing.Point(359, 222);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(142, 36);
+            this.num5.TabIndex = 12;
+            this.num5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(276, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 36);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "คำนวน";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(400, 281);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 36);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // seri
+            // 
+            this.seri.AutoSize = true;
+            this.seri.Font = new System.Drawing.Font("Kanit", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seri.ForeColor = System.Drawing.Color.White;
+            this.seri.Location = new System.Drawing.Point(156, 102);
+            this.seri.Name = "seri";
+            this.seri.Size = new System.Drawing.Size(122, 34);
+            this.seri.TabIndex = 15;
+            this.seri.TabStop = true;
+            this.seri.Text = "แบบอนุกรม";
+            this.seri.UseVisualStyleBackColor = true;
+            // 
+            // para
+            // 
+            this.para.AutoSize = true;
+            this.para.Font = new System.Drawing.Font("Kanit", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.para.ForeColor = System.Drawing.Color.White;
+            this.para.Location = new System.Drawing.Point(296, 102);
+            this.para.Name = "para";
+            this.para.Size = new System.Drawing.Size(108, 34);
+            this.para.TabIndex = 16;
+            this.para.TabStop = true;
+            this.para.Text = "แบบขนาน";
+            this.para.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(120, 339);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(79, 33);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "ผลลัพธ์";
+            // 
+            // ans
+            // 
+            this.ans.BackColor = System.Drawing.Color.White;
+            this.ans.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ans.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(3)))), ((int)(((byte)(3)))), ((int)(((byte)(3)))));
+            this.ans.Location = new System.Drawing.Point(228, 339);
+            this.ans.Name = "ans";
+            this.ans.Size = new System.Drawing.Size(149, 33);
+            this.ans.TabIndex = 18;
+            this.ans.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(394, 339);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(95, 33);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Ω (Ohm)";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(507, 164);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(31, 33);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Ω";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.White;
+            this.label12.Location = new System.Drawing.Point(507, 222);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(31, 33);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "Ω";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.Color.White;
+            this.label13.Location = new System.Drawing.Point(245, 167);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(31, 33);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "Ω";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.Color.White;
+            this.label14.Location = new System.Drawing.Point(245, 225);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(31, 33);
+            this.label14.TabIndex = 23;
+            this.label14.Text = "Ω";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Kanit", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.ForeColor = System.Drawing.Color.White;
+            this.label15.Location = new System.Drawing.Point(245, 279);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(31, 33);
+            this.label15.TabIndex = 24;
+            this.label15.Text = "Ω";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(567, 392);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.ans);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.para);
+            this.Controls.Add(this.seri);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.num5);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num4);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "resistor_cal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.TextBox num4;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.TextBox num3;
+        private System.Windows.Forms.TextBox num5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton seri;
+        private System.Windows.Forms.RadioButton para;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label ans;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+    }
+}
+
---
+++ Lab3/.Net-Lab3_6.5/Lab3_6.5/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3_6._5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (seri.Checked) 
+            {
+                ans.Text = (float.Parse(num1.Text) +
+                        float.Parse(num2.Text) +
+                        float.Parse(num3.Text) +
+                        float.Parse(num4.Text) +
+                        float.Parse(num5.Text) ).ToString();
+            }
+                
+            if (para.Checked) 
+            {
+                ans.Text = (1.0 / ((1.0 / float.Parse(num1.Text)) +
+                    (1.0 / float.Parse(num2.Text)) +
+                    (1.0 / float.Parse(num3.Text)) +
+                    (1.0 / float.Parse(num4.Text)) +
+                    (1.0 / float.Parse(num5.Text)))).ToString();
+            }   
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TextBox[] num = { num1, num2, num3, num4, num5 };
+            for (int i = 0; i < num.Length; i++)
+                num[i].Text = "";
+        } 
+    }
+}
